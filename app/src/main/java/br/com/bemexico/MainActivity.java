ba2f46b33ad2b8b6dac9f3c2f900d9e713171653
Thiago Marques <tm.uea.2014@gmail.com>
2016-08-29 23:55:12 -0400

    Primeiro protótipo da seleção de imagens e a partir da cor selecionada executa uma ação x. Falta terminar a tela de detalhes e mapear o que será mostrado

--- app/src/main/java/br/com/bemexico/MainActivity.java
+++ app/src/main/java/br/com/bemexico/MainActivity.java
@@ -1,15 +1,33 @@
 package br.com.bemexico;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.nguyenhoanglam.imagepicker.activity.ImagePicker;
+import com.nguyenhoanglam.imagepicker.activity.ImagePickerActivity;
+import com.nguyenhoanglam.imagepicker.model.Image;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_PICKER = 1;
+    private static final int CAMERA_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +40,87 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                CAMERA_PERMISSION);
+                    }else{
+                        selectImage();
+                    }
+                }else{
+                    selectImage();
+                }
             }
         });
+    }
+
+    private void selectImage() {
+        ImagePicker.create(MainActivity.this)
+                .single() // single mode
+                .limit(10) // max images can be selected
+                .showCamera(true) // show camera or not (true by default)
+                .start(REQUEST_CODE_PICKER); // start image picker activity with request code
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Now user should be able to use camera
+                selectImage();
+            }
+            else {
+                // Your app will not have this permission. Turn off all functions
+                // that require this permission or it will force close like your
+                // original question
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICKER && resultCode == RESULT_OK && data != null) {
+            ArrayList<Image> images = data.getParcelableArrayListExtra(ImagePickerActivity.INTENT_EXTRA_SELECTED_IMAGES);
+            Log.d("Image", "Imagem recebida");
+            // do your logic ....
+
+            for(Image image : images){
+                Bitmap bitmap = BitmapFactory.decodeFile(image.getPath());
+                // Asynchronous
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    public void onGenerated(Palette p) {
+                        // Use generated instance
+                        int color = 0x000000;
+                        int vibrant = p.getVibrantColor(color);
+
+                        TextView colorV = (TextView) findViewById(R.id.color);
+                        colorV.setBackgroundColor(vibrant);
+
+                        Log.d("Color", vibrant + "");
+
+                        delegateAction(vibrant);
+                    }
+                });
+            }
+
+        }
+    }
+
+    private void delegateAction(int vibrant) {
+        switch (vibrant){
+            case -13618952 : // C1
+                Log.d("Color", "Circle blue");
+                break;
+            case -485376 : // C2
+                Log.d("Color", "Circle orange");
+                break;
+        }
     }
 
     @Override
